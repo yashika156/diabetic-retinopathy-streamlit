d42b947b5d81f3ba73531e8756119dc92e136240
yashika156 <yashikasengar4@gmail.com>
2025-04-05 01:25:06 +0530

    main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from keras.models import load_model
+import gdown
+
+# Download from public Google Drive link (shared with 'Anyone')
+url = "https://drive.google.com/uc?id=YOUR_FILE_ID"
+output = "model/dr_cnn_model.h5"
+gdown.download(url, output, quiet=False)
+
+model = load_model(output)
