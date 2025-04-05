c706a5313277d40faea6ccbaa8a59cb093ee85ae
yashika156 <yashikasengar4@gmail.com>
2025-04-06 01:09:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from keras.models import load_model
 import gdown
+import os
 
+# Ensure 'model' directory exists
+os.makedirs("model", exist_ok=True)
 # Download from public Google Drive link (shared with 'Anyone')
 url = url = "https://drive.google.com/uc?export=download&id=1dCDPNXxSz3Kv9X5uS6iknfs01EU5tzQJ"
 output = "model/dr_cnn_model.h5"
