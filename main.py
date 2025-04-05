9805b9576f62a86f08c63fc9529041a171c36400
yashika156 <yashikasengar4@gmail.com>
2025-04-06 00:59:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ from keras.models import load_model
 import gdown
 
 # Download from public Google Drive link (shared with 'Anyone')
-url = "https://drive.google.com/uc?id=1dCDPNXxSz3Kv9X5uS6iknfs01EU5tzQJ"
+url = "https://drive.google.com/uc?id="1dCDPNXxSz3Kv9X5uS6iknfs01EU5tzQJ/view?usp=sharing"
 output = "model/dr_cnn_model.h5"
 gdown.download(url, output, quiet=False)
 
