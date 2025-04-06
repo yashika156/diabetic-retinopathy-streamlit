929cf9005c0e7470d185bd8dfc2e44ffeb62c3ac
yashika156 <yashikasengar4@gmail.com>
2025-04-06 12:59:38 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
+import streamlit as st
+import numpy as np
+from PIL import Image
 from keras.models import load_model
-import gdown
-import os
 
-# Ensure 'model' directory exists
-os.makedirs("model", exist_ok=True)
-# Download from public Google Drive link (shared with 'Anyone')
-url = url = "https://drive.google.com/uc?export=download&id=1dCDPNXxSz3Kv9X5uS6iknfs01EU5tzQJ"
-output = "model/dr_cnn_model.h5"
-gdown.download(url, output, quiet=False)
+model = load_model("model/dr_cnn_model.h5")
+st.title("🧠 Diabetic Retinopathy Classifier")
 
-model = load_model(output)
+uploaded = st.file_uploader("Upload Retina Image", type=["jpg", "png", "jpeg"])
+if uploaded:
+    image = Image.open(uploaded).resize((224, 224))
+    st.image(image, caption="Uploaded Image", use_column_width=True)
+
+    img_array = np.expand_dims(np.array(image)/255.0, axis=0)
+    pred = model.predict(img_array)
+    stage = ["No DR", "Mild", "Moderate", "Severe", "Proliferative DR"][np.argmax(pred)]
+
+    st.subheader(f"Prediction: {stage}")
+
